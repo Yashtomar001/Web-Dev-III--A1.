0625de0b4cc10a693a00ec491a349b323f52569e
Yashtomar001 <tomaryash2007@gmail.com>
2026-08-28 11:31:40 +0530

    first commit

---
+++ dice.js
@@ -0,0 +1,16 @@
+// const crypto = require("crypto");
+
+// function rollDice() {
+//     return crypto.randomInt(1, 7);
+// }
+
+// console.log("Dice Rolled:", rollDice());
+const crypto = require("crypto");
+
+function rollDice() {
+    return crypto.randomInt(1, 7);
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(`Dice ${i}:`, rollDice());
+}
